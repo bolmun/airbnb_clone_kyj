1346d65eee4e57e7bb83f8af4bf5a3e2cff0fc70
Yoojung Kim <apex.yj@gmail.com>
2020-03-14 18:18:56 +0900

    Update models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from core import models as core_models
-from users import models as user_models
 
 
 class Review(core_models.TimeStampedModel):
@@ -14,4 +13,5 @@ class Review(core_models.TimeStampedModel):
     accuracy = models.IntegerField()
     cleanliness = models.IntegerField()
     value = models.IntegerField()
-    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
